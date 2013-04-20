ee95c08ca6d23d30ae1510557c97f21a81e1dad3
mtrpires <marco@bitcount.com.br>
2013-04-19 23:16:40 -0300

    Added functions and stalker scripts

---
+++ functions.py
@@ -0,0 +1,201 @@
+#!/usr/local/bin/python
+# -*- coding: UTF-8 -*-
+# Rastreador de presidente / governador?
+# @mtrpires
+
+import urllib
+import json
+from bs4 import BeautifulSoup
+
+class Candidate(object):
+    """
+    Initiates a Candidate instance, saves all parameters
+    as atributes of the instance.
+    name: a string containing the candidate's real name
+    twitterURL: a string containing the candidate's twitter URL
+    facebookID: a string containing the candidate's fbID
+    website: a string containing the candidate's official website
+    wiki: a string containing the Wikipedia (pt) entry for the candidate
+    """
+
+    def __init__(self, name, twitterName, facebookID, website, wiki):
+        self.name = name
+        self.twitterName = twitterName
+        self.facebookID = facebookID
+        self.website = website
+        self.wiki = wiki
+
+    def getName(self):
+        """
+        Returns the candidate's name
+        """
+        return self.name
+
+    def setTwitter(self, twitterName):
+        """
+        Sets twitterName for candidate
+        """
+        self.twitterName = twitterName
+        print self.name, " - twitterName set to:", self.twitterName
+
+    def getTwitter(self):
+        """
+        Returns the candidate's twitter profile name
+        """
+        return self.twitterName
+
+    def setFacebook(self, facebookID):
+        """
+        Sets Facebook ID for candidate
+        """
+        self.facebookID = facebookID
+        print self.name, " - facebookID set to:", self.facebookID
+
+    def getFacebook(self):
+        """
+        Returns the candidate's Facebook ID
+        """
+        return self.facebookID
+
+    def setWebsite(self, website):
+        """
+        Sets website for candidate
+        """
+        self.website = website
+        print self.name, " - website set to:", self.website
+        
+    def getWebsite(self):
+        """
+        Returns the candidate's website
+        """
+        return self.website
+
+    def setFacebook(self, wiki):
+        """
+        Sets Wikipedia page (PT-BR) for candidate
+        """
+        self.wiki = wiki
+        print self.name, " - Wikipedia page set to:", self.wiki
+        
+    def getWiki(self):
+        """
+        Returns the Wikipedia entry on the candidate (PT-BR)
+        """
+        return self.wiki
+
+    def getNumFollowers(self):
+        """
+        Uses the helper function getTwitterDump to extract
+        the number of followers from the JSON dictionary.
+
+        returns: the number of followers of a given user.
+        """
+
+        return getTwitterDump(self)[u'followers_count']
+
+    def getNumTweets(self):
+        """
+        Uses the helper function getTwitterDump to extract
+        the number of tweets from the JSON dictionary.
+
+        returns: the number (int) of tweets of a given user.
+        """
+
+        return getTwitterDump(self)[u'statuses_count']
+
+    def getLatestTweets(self):
+        """
+        Uses the helper function getTwitterDump to extract the X
+        latest tweets from a given user.
+
+        returns: list of tweets. Each index is a tweet.
+        """
+
+        return getTwitterDump(self)
+
+    def getNumLikes(self):
+        """
+        Uses the helper function getFacebookDump to extract
+        the number of likes from the JSON dictionary.
+
+        returns: the number (int) of likes of a given Facebook page.
+        """
+        try:
+            friendCount = getFacebookDump(self)[u'likes']
+            
+        except KeyError:
+            dump = json.loads(urllib.urlopen(\
+            "https://graph.facebook.com/fql?q=SELECT%20friend_count%20FROM%20user%20WHERE%20uid="\
+            +self.getFacebook()).readline())
+            friendCount = dump[u'data'][0][u'friend_count']
+            
+        return friendCount
+            
+    def getStories(self):
+        """
+        Goes to Folha.com and searches for 'candidate'.
+        Returns the last three stories where his/her name shows up.
+
+        returns: list with HTML for stories.
+        """
+
+        results = \
+        "http://search.folha.com.br/search?q=%s&site=online" % self.name
+        soupResults = BeautifulSoup(urllib.urlopen(results).read())
+        resultsList = soupResults.findAll('p')
+
+        return resultsList[2:5]
+
+    def __repr__(self):
+        """
+        Shows useful information about the Object when called from prompt
+        """
+        return self.getName()
+
+
+def getTwitterDump(candidate):
+    """
+    Gets the JSON string for 'candidate.getTwitter()' and defined
+    type (status or general information) and converts
+    it into a dictionary (general) or list (status)
+    using json.loads. Default value for latest tweets is 3.
+
+    candidate: an instance of the object Candidate.
+
+    returns: dictionary with JSON items as keys and its
+    respective values.
+    """
+
+    twitterJSON = \
+    "https://api.twitter.com/1/statuses/user_timeline.json?\
+    include_rts=true&screen_name=%s"\
+    % (candidate.getTwitter())
+
+    dump = json.loads(urllib.urlopen(twitterJSON).readline())
+    return dump
+
+
+def getFacebookDump(candidate):
+    """
+    Gets the JSON string for 'facebookID' and converts
+    it into a dictionary using json.loads.
+
+    returns: dictionary with JSON items as keys and its
+    respective values.
+    """
+    
+    facebookJSON = \
+    "http://graph.facebook.com/%s/" % candidate.getFacebook()
+    
+    dumpDict = json.loads(urllib.urlopen(facebookJSON).readline())
+    
+    return dumpDict
+
+#Intencoo de voto
+#Ultima pesquisa datafolha? Medias das ultimas pesquisas?
+
+#Dinheiro arrecadado
+#Declarado no governo federal. Site? DB?
+
+#Principais doadores
+#Exibidos no site do governo federal. Site? DB?
---
+++ stalker.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python
+# -*- coding: UTF-8 -*-
+# Rastreador de presidente / governador?
+# @mtrpires
+
+import cgitb
+cgitb.enable()
+
+from rastFunctions import Candidate
+
+dilma = Candidate('Dilma Rousseff', 'dilmabr', '112476482098688',
+                  'http://www.dilma.com.br/',
+                  'http://pt.wikipedia.org/wiki/Dilma_Rousseff')
+                  
+aecio = Candidate('Aecio Neves', 'aecioneves', '411754008869486',
+                  'http://www.aecioneves.com.br/',
+                  'http://pt.wikipedia.org/wiki/Aecio_Neves')
+                  
+eduardo = Candidate('Eduardo Campos', 'eduardocampos40',
+                    '100001008251772',
+                    'http://www.eduardocampos40.com.br/',
+                    'http://pt.wikipedia.org/wiki/Eduardo_campos')
+                    
+marina = Candidate('Marina Silva', 'silva_marina', '126351747376464',
+                   'http://www.minhamarina.org.br/',
+                   'http://pt.wikipedia.org/wiki/Marina_Silva')
+
+candidatos = [dilma, aecio, eduardo, marina]
+
+print "Content-type: text/html"
+print 
+print "<html>"
+print "<body><p>"
+
+for i in range(len(candidatos)):
+    
+    print "<h1>", candidatos[i].getName(), "</h1><h2>", candidatos[i].getNumFollowers(), "</h2> seguidores no twitter. </p><p>"
+    print "Website:", candidatos[i].getWebsite(), "</p><p>"
+    print "Wikipedia:", candidatos[i].getWiki(), "</p>"
+    print "<h2>", candidatos[i].getNumTweets(), "</h2> tweets. </p><p>"
+    print "<h2>", candidatos[i].getNumLikes(), "</h2> seguidores no Facebook. </p><p>"
+    print
+    print
+    print "Últimos tweets: </p><p>"
+
+    if candidatos[i].getNumTweets() == 0:
+        print "Nenhum Tweet!</p><p>"
+    else: 
+        tweets = candidatos[i].getLatestTweets()
+        for j in range(3):
+            print "<h3>", tweets[j][u'text'].encode('utf_8'),"</h3></p><p>"
+            
+    print
+    print "Últimas reportagens sobre", candidatos[i], "</p>"
+    print
+    news = candidatos[i].getStories()
+    for k in range(3):
+        print news[k].encode('utf_8')
+
+print "</p></body>"
+print "</html>"
